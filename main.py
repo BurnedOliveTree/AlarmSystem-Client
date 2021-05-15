33b069d7656bc395ae24c9e58669ddacd0d07e56
Paweł Muller <pawelmuller42@gmail.com>
2021-05-15 22:27:07 +0200

    Added possibility to upload file to server.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from threading import Thread
 from time import sleep
 from pyaudio import PyAudio, paInt16
 import wave
+import requests
+
 
 class Recorder:
     FORMAT = paInt16    # data format
@@ -19,7 +21,8 @@ class Recorder:
     def start(self):
         self.audio = PyAudio()
         self.frames = []
-        self.stream = self.audio.open(format=Recorder.FORMAT, channels=Recorder.CHANNELS, rate=Recorder.RATE, input=True, frames_per_buffer=Recorder.CHUNK)
+        self.stream = self.audio.open(format=Recorder.FORMAT, channels=Recorder.CHANNELS, rate=Recorder.RATE,
+                                      input=True, frames_per_buffer=Recorder.CHUNK)
         self.recording = True
         while self.recording:
             self.frames.append(self.stream.read(Recorder.CHUNK))
@@ -38,22 +41,32 @@ class Recorder:
             waveFile.setframerate(Recorder.RATE)
             waveFile.writeframes(b''.join(self.frames))
 
+    @staticmethod
+    def upload(ip="0.0.0.0"):
+        with open('file.wav', 'rb') as waveFile:
+            files = {"record": waveFile}
+            requests.post(f"http://{ip}/device/upload-record", files=files)
+
     def terminal(self):
         i = ''
         while i != 'end':
             i: str = input('python$ ')
             if i == 'start':
-                t = Thread(target=self.start)
-                t.start()
+                recording_thread = Thread(target=self.start)
+                recording_thread.start()
                 print('Started recording!')
             if i == 'stop':
                 self.stop()
-                print('Stoped recording!')
+                print('Stopped recording!')
             if i == 'save':
                 self.save()
                 print('Audio saved!')
+            if i == 'upload':
+                self.upload()
+                print('Audio uploaded to server!')
             if i == 'end':
                 break
+
 
 if __name__ == '__main__':
     record = Recorder()
